d6af93e7489241cca0be871849bed94b89ddc1ad
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-13 20:54:28 +0300

    commit migrations2

--- MuzikStoreRazor/MuzikStoreRazor.UI/Pages/MusicList/Create.cshtml.cs
+++ MuzikStoreRazor/MuzikStoreRazor.UI/Pages/MusicList/Create.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MuzikStoreRazor.UI.Models;
 
 namespace MuzikStoreRazor.UI.Pages.MusicList
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly MusicStorelistContext _db;
+
+        public CreateModel(MusicStorelistContext db)
         {
+            _db = db;
         }
+
+        public IEnumerable<Music> Musics { get; set; }
+
+        public async Task OnGet()
+        {
+            Musics = await _db.Musics.ToListAsync();
+        }
+
+       
     }
 }
